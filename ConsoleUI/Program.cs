d01647ca3c4bc3709f1e1c612d4578f29e8baa44
hp <hp@DESKTOP-U5VSIRJ>
2021-09-09 13:37:29 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            InMemoryProductDal InMermoryProductDal = new InMemoryProductDal();
+            ProductManager productManager = new ProductManager(InMermoryProductDal);
+
+            foreach (var item in productManager.GetAll()) 
+            {
+                Console.WriteLine(item.ProductId +" --- "  + item.ProductName); 
+            }
+        }
+    }
+}
